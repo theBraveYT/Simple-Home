cb4becfbfa1a66f70c93ca6fb78c97044688714e
theBraveYT <98583204+theBraveYT@users.noreply.github.com>
2022-03-13 12:07:36 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,13 +14,17 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
 	@Override
-	public void onEnable() {
+	public void onEnable() {			
 		this.getDataFolder().mkdirs();
 		try {
 			File dataFile = new File(this.getDataFolder().getPath()+"/data.txt");
@@ -31,6 +35,7 @@ public class Main extends JavaPlugin {
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+		Bukkit.getPluginManager().registerEvents(this, this);
 		readData();
 	}
 	
@@ -38,6 +43,16 @@ public class Main extends JavaPlugin {
 	public void onDisable() {
 		saveData();
 	}
+	
+	@EventHandler
+	public void onPlayerDeath(EntityDeathEvent event) {
+		if(event.getEntityType().equals(EntityType.PLAYER)) {
+			Player p = (Player) event.getEntity();
+			p.performCommand("sethome death");
+		}
+	}
+	
+	String STOP = "STOP";
 	
 	public void readData() {
 		File dataFile = new File(this.getDataFolder().getPath()+"/data.txt");
@@ -57,8 +72,19 @@ public class Main extends JavaPlugin {
 				for(String s1 : words1) {
 					words.add(s1);
 				}
-				Location loc = new Location(Bukkit.getWorld(words.get(1)), Integer.valueOf(words.get(2)), Integer.valueOf(words.get(3)), Integer.valueOf(words.get(4)));
-				homes.put(words.get(0), loc);
+				HashMap<String,Location> HOMES = new HashMap<String,Location>();
+				String UUID = words.get(0);
+				for(int i = 1; i < words.size(); i = i + 0) {
+					if(words.get(i).equals(STOP) || words.get(i+1).equals(STOP) || words.get(i+2).equals(STOP) || words.get(i+3).equals(STOP) || words.get(i+4).equals(STOP)) {
+						break;
+					}
+					else {
+						Location loc = new Location(Bukkit.getWorld(words.get(i+1)), Integer.valueOf(words.get(i+2)), Integer.valueOf(words.get(i+3)), Integer.valueOf(words.get(i+4)));
+						HOMES.put(words.get(i), loc);
+						i = i + 5;
+					}
+				}
+				homes.put(UUID, HOMES);
 			}
 			br.close();
 		}
@@ -70,10 +96,17 @@ public class Main extends JavaPlugin {
 	public void saveData() {
 		File dataFile = new File(this.getDataFolder().getPath()+"/data.txt");
 		try {
+			dataFile.delete();
+			dataFile.createNewFile();
 			BufferedWriter bw = new BufferedWriter(new FileWriter(dataFile));
-			for(Entry<String, Location> e : homes.entrySet()) {
-				Location loc = e.getValue();
-				bw.write(e.getKey() + " " + loc.getWorld().getName() + " " + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ());
+			for(Entry<String, HashMap<String, Location>> e : homes.entrySet()) {
+				String toWrite = "";
+				toWrite = toWrite + e.getKey() + " ";
+				for(Entry<String, Location> entry : e.getValue().entrySet()) {
+					Location loc = entry.getValue();
+					toWrite = toWrite + entry.getKey() + " " + loc.getWorld().getName() + " " + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ() + " ";
+				}
+				bw.write(toWrite + " " + STOP);
 				bw.newLine();
 			}
 			bw.close();
@@ -83,7 +116,7 @@ public class Main extends JavaPlugin {
 		}
 	}
 	
-	HashMap<String, Location> homes = new HashMap<String, Location>();
+	HashMap<String, HashMap<String,Location>> homes = new HashMap<String, HashMap<String,Location>>();
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -94,49 +127,90 @@ public class Main extends JavaPlugin {
 		Player p = (Player) sender;
 		String uuid = p.getUniqueId().toString();
 		Location loc = p.getLocation();
-		if(label.equalsIgnoreCase("sethome")) {
-			if(!containsUUID(uuid)) {
-				homes.put(uuid, loc);
+		if(homes.get(uuid)==null||homes.get(uuid).isEmpty()) {
+			homes.put(uuid, new HashMap<String,Location>());
+		}
+		if(label.equalsIgnoreCase("homes")) {
+			p.sendMessage(ChatColor.GOLD + "Home data formatting:");
+			p.sendMessage(ChatColor.GOLD + "<home-name> " + ChatColor.GREEN + "<world-name> " + ChatColor.BLUE + "<x> <y> <z>");
+			for(Entry<String, HashMap<String, Location>> e : homes.entrySet()) {
+				if(e.getKey().equals(uuid)) {
+					for(Entry<String,Location> entry : e.getValue().entrySet()) {
+						p.sendMessage(ChatColor.GOLD + entry.getKey() + " " + ChatColor.GREEN + entry.getValue().getWorld().getName() + " " + 
+								ChatColor.BLUE + entry.getValue().getBlockX() + " " + entry.getValue().getBlockY() + " " + entry.getValue().getBlockZ());
+					}
+				}
+ 			}
+			p.sendMessage(ChatColor.GREEN + "Listed all homes successfully!");
+			return true;
+		}
+		else if(args.length!=1) {
+			  p.sendMessage(ChatColor.RED + "You need to specify one home you want to set, delete or go to! Do /homes to see a list!");
+			  return true;
+		}
+		else if(label.equalsIgnoreCase("sethome")) {
+			String homeName = args[0];
+			HashMap<String,Location> HOMES = homes.get(uuid);
+			if(HOMES==null) {
+				HashMap<String, Location> hash = new HashMap<String,Location>();
+				homes.put(uuid, hash);
+				HOMES = hash;
+			}
+			if(containsHome(homeName, HOMES)) {
+				HOMES.replace(homeName, loc);
 			}
 			else {
-				homes.replace(uuid, loc);
+				HOMES.put(homeName, loc);
 			}
-			p.sendMessage(ChatColor.GREEN + "Set your home location to " + loc.getBlockX() + " " + loc.getBlockY()
-			+ " " + loc.getBlockZ() + " in world " + loc.getWorld().getName());
+			homes.replace(uuid, HOMES);
+			p.sendMessage(ChatColor.GREEN + "Set home " + homeName + " to the location: " + 
+					loc.getWorld().getName() + " " + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ());
+			return true;
 		}
-		if(label.equalsIgnoreCase("home")) {
-			p.sendMessage(ChatColor.GOLD + "Teleporting you to your home in 5 seconds, don't move!");
-			int blockX = loc.getBlockX();
-			int blockY = loc.getBlockY();
-			int blockZ = loc.getBlockZ();
+		else if(label.equalsIgnoreCase("delhome")) {
+			String homeName = args[0];
+			HashMap<String,Location> HOMES = homes.get(uuid);
+			if(HOMES==null) {
+				HashMap<String, Location> hash = new HashMap<String,Location>();
+				homes.put(uuid, hash);
+				HOMES = hash;
+			}
+			HOMES.remove(homeName);
+			homes.replace(uuid, HOMES);
+			p.sendMessage(ChatColor.GREEN + "Deleted home " + homeName);
+		}
+		else if(label.equalsIgnoreCase("home")) {
+			String homeName = args[0];
+			p.sendMessage(ChatColor.GOLD + "Teleporting you to home " + homeName + " in 5 seconds - don't move!");
+			int X = loc.getBlockX();
+			int Y = loc.getBlockY();
+			int Z = loc.getBlockZ();
 			Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 				@Override
 				public void run() {
+					p.sendMessage(ChatColor.GOLD + "Telporting...");
 					Location loc2 = p.getLocation();
 					int x = loc2.getBlockX();
 					int y = loc2.getBlockY();
 					int z = loc2.getBlockZ();
-					boolean shouldContinue = false;
-					if(x==blockX) {
-						if(y == blockY) {
-							if(z == blockZ) {
-								shouldContinue = true;
-							}
+					if(X==x&&Y==y&&Z==z) {
+						HashMap<String,Location> HOMES = homes.get(uuid);
+						if(HOMES==null) {
+							HashMap<String, Location> hash = new HashMap<String,Location>();
+							homes.put(uuid, hash);
+							HOMES = hash;
 						}
-					}
-					if(shouldContinue) {
-						p.sendMessage(ChatColor.GREEN + "Teleporting...");
-						if(containsUUID(uuid)) {
-							Location homeLoc = homes.get(uuid);
-							p.teleport(homeLoc);
-							p.sendMessage(ChatColor.GOLD + "Welcome to your home!");
+						Location homeLoc = HOMES.get(homeName);
+						if(homeLoc==null) {
+							p.sendMessage(ChatColor.RED + "That home doesn't exist!");
 						}
 						else {
-							p.sendMessage(ChatColor.RED + "You don't have a home!");
+							p.teleport(homeLoc);
+							p.sendMessage(ChatColor.GREEN + "Welcome to " + homeName + "!");
 						}
 					}
 					else {
-						p.sendMessage(ChatColor.RED + "You moved! Teleportation cancelled!");
+						p.sendMessage(ChatColor.RED + "You moved!");
 					}
 				}
 			},100);
@@ -145,14 +219,27 @@ public class Main extends JavaPlugin {
 	}
 	
 	
-	public boolean containsUUID(String uuid) {
+	public boolean containsUUID(String string) {
 		boolean value = false;
-		for(Entry<String, Location> e : homes.entrySet()) {
-			if(e.getKey().equals(uuid)) {
+		for(Entry<String, HashMap<String, Location>> e : homes.entrySet()) {
+			if(e.getKey().equals(string)) {
 				value = true;
 				break;
 			}
 		}
 		return value;
 	}
+	public boolean containsHome(String string, HashMap<String,Location> HOMES) {
+		boolean value = false;
+		for(Entry<String, Location> e : HOMES.entrySet()) {
+			if(e.getKey().equals(string)) {
+				value = true;
+				break;
+			}
+		}
+		return value;
+	}
+	
+	//p.sendMessage(ChatColor.GOLD + "Do /homes to see a list of your homes, /delhome to delete it, /sethome to overwrite or /home " + homeName + " to go there!");
+
 }
